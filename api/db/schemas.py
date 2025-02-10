1139c3a1d5255d9ced11430b9c23b7fd22921b21
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-10 22:30:26 +0000

    feat: Update schema with new models, accurate types and return types

--- api/db/schemas.py
+++ api/db/schemas.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import OrderedDict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 
 
 class Genre(str, Enum):
@@ -16,7 +16,7 @@ class Genre(str, Enum):
 
 
 class Book(BaseModel):
-    """Book schema
+    """Book schema.
 
     Args:
         BaseModel (BaseModel): Pydantic base model.
@@ -28,18 +28,73 @@ class Book(BaseModel):
     publication_year: int
     genre: Genre
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 1,
+                "title": "The Hobbit",
+                "author": "J.R.R. Tolkien",
+                "publication_year": 1937,
+                "genre": "Fantasy",
+            }
+        }
+    }
+
+
+class BooksResponse(RootModel[OrderedDict[int, Book]]):
+    """This model defines the schema for how multiple books will look in a
+    response.
+
+    Args:
+        RootModel (RootModel): Pydantic root model.
+    """
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "1": {
+                    "id": 1,
+                    "title": "The Hobbit",
+                    "author": "J.R.R. Tolkien",
+                    "publication_year": 1937,
+                    "genre": "Fantasy",
+                },
+                "2": {
+                    "id": 2,
+                    "title": "The Lord of the Rings",
+                    "author": "J.R.R. Tolkien",
+                    "publication_year": 1954,
+                    "genre": "Fantasy",
+                },
+                "3": {
+                    "id": 3,
+                    "title": "The Return of the King",
+                    "author": "J.R.R. Tolkien",
+                    "publication_year": 1955,
+                    "genre": "Fantasy",
+                },
+            }
+        }
+    }
+
+
+class BookNotFound(BaseModel):
+    """Define the schema for books not found response."""
+
+    detail: str = "Book not found"
+
 
 class InMemoryDB:
     def __init__(self):
-        self.books: OrderedDict[int, Book] = {}
+        self.books: OrderedDict[int, Book] = OrderedDict()
 
-    def get_books(self) -> OrderedDict[int, Book]:
+    def get_books(self) -> BooksResponse:
         """Gets books from database.
 
         Returns:
-            OrderedDict[int, Book]: Ordered dictionary of books.
+            BooksResponse: Ordered dictionary of books.
         """
-        return self.books
+        return BooksResponse(root=self.books)
 
     def add_book(self, book: Book) -> Book:
         """Adds book to database.
@@ -51,8 +106,9 @@ class InMemoryDB:
             Book: Added book.
         """
         self.books.update({book.id: book})
+        return book
 
-    def get_book(self, book_id: int) -> Book:
+    def get_book(self, book_id: int) -> Book | None:
         """Gets a specific book from database.
 
         Args:
@@ -63,7 +119,7 @@ class InMemoryDB:
         """
         return self.books.get(book_id)
 
-    def update_book(self, book_id: int, data: Book) -> Book:
+    def update_book(self, book_id: int, data: Book) -> Book | None:
         """Updates a specific book in database.
 
         Args:
